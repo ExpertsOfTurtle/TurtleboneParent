816b78a8c930606433938d27ae48d9ddb630d44b
danny01.feng <danny01.feng@vipshop.com>
2017-12-25 18:49:45 +0800

    希望这次可以啦

--- TurtleboneHome/src/main/java/com/turtlebone/home/HomeApp.java
+++ TurtleboneHome/src/main/java/com/turtlebone/home/HomeApp.java
@@ -1,11 +1,15 @@
 package com.turtlebone.home;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.turtlebone")
+@MapperScan("com.turtlebone.*.repository")
+@EnableScheduling
 @EnableAutoConfiguration
 public class HomeApp {
 	public static void main(String[] args) {
