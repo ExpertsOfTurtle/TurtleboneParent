ee232c1bf33ed556b592c97e0fea008500f33986
danny01.feng <danny01.feng@vipshop.com>
2017-12-26 17:59:18 +0800

    我本地是可以的！！！

--- TurtleboneAuth/src/main/java/com/turtlebone/auth/AuthApplication.java
+++ TurtleboneAuth/src/main/java/com/turtlebone/auth/AuthApplication.java
@@ -4,8 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.turtlebone.auth")
-@MapperScan("com.turtlebone.auth.repository") 
+@SpringBootApplication(scanBasePackages = "com.turtlebone")
+@MapperScan("com.turtlebone.*.repository") 
 public class AuthApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
